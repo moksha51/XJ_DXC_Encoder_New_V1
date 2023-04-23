ab7fede4f65545ce05b8f43d7da1bef142e7a28e
moksha51 <103093263+moksha51@users.noreply.github.com>
2023-04-24 00:11:31 +0800

    Add files via upload
    Initial Commit

---
+++ DXC/src/App.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+
+public class App {
+
+    // I can make it into classes but I thought for reading purposes I think it's
+    // easier to read in this state.
+
+    static char[] referenceTable = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '*',
+            '+', ',', '-', '.', '/' };
+    private static char offSet = 'B';
+
+    public static void main(String[] args) throws Exception {
+        String plainText = "Hello World";
+        encode(plainText);
+        String encodedText1 = "FC/GGJ RJMG.";
+        String encodedText2 = "BGDKKN VNQKC";
+        decode(encodedText2);
+
+    }
+
+    public static String encode(String plainText) {
+        // using common code cause same logic
+        return App.offSet + App.commonCode(true, plainText);
+    }
+
+    public static String decode(String encodedText) {
+        // using common code cause same logic but reversed, just need to get the first
+        // char.
+        App.offSet = App.getFirstChar(encodedText);
+        String newText = encodedText.substring(1);
+        return commonCode(false, newText);
+    }
+
+    public static int findReferenceTableIndex(char[] referenceTable, char offSet) {
+        // to decouple from common code
+        int index = 0;
+        for (int i = 0; i < referenceTable.length; i++) {
+            if (referenceTable[i] == offSet) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static char getFirstChar(String encodedText) {
+        char firstChar = encodedText.charAt(0);
+        return firstChar;
+    }
+
+    public static String removeFirstChar(String encodedText) {
+        return encodedText.substring(1);
+    }
+
+    public static String commonCode(Boolean isEncode, String text) {
+        char[] arr = text.toUpperCase().toCharArray();
+        char[] newArr = new char[text.length()];
+        int diff, turnaround, jumps;
+        jumps = App.findReferenceTableIndex(App.referenceTable, App.offSet);
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < referenceTable.length; j++) {
+                if (arr[i] == referenceTable[j]) {
+                    if (isEncode == true) {
+                        diff = j - jumps;
+                    } else {
+                        diff = j + jumps;
+                    }
+                    if (diff > 0 && diff < referenceTable.length) {
+                        newArr[i] = referenceTable[diff];
+                    } else if (diff < 0) {
+                        turnaround = referenceTable.length + diff;
+                        newArr[i] = referenceTable[turnaround];
+                    } else if (diff > referenceTable.length) {
+                        turnaround = diff - referenceTable.length;
+                        newArr[i] = referenceTable[turnaround];
+                    }
+                }
+            }
+        }
+        String encodedtext = new String(newArr);
+        System.out.println(encodedtext);
+        return encodedtext;
+    }
+}
